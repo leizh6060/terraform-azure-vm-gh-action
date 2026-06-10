9a99f56eca806e56ab69b8cb10e59ea9def350d5
leizh6060 <leizh6060@yahoo.com>
2026-06-10 14:31:53 -0400

    vm version change

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "ubuntu-server"
-    sku       = "22.04-LTS"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
